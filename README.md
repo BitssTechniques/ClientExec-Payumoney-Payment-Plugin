8f33c73cb7b524dc2a18f3851d30576f071a6182
amit <mittal>
2022-04-11 19:04:22 +0530

    initial

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ClientExec-Payumoney-Payment-Plugin
 Payumoney integration for ClientExec Web Hosting Billing System
+
+Installation:
+1) Download the Zip
+2) Upload and extract zip to /plugins/gateways/
+3) Login ClientExec backend as admin, Go to Settings->Plugins->Payment Processors
+4) Select "Payu" from the dowpdown and configure your keys and other settings.
+5) That's it. Enjoy!
---
+++ callback.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ *
+ * ClientExec Plugin
+ *
+ * ClientExec Payumoney Payment Gateway Plugin
+ *
+ * @Author : Amit Mittal
+ * 
+ * Version : 1.0
+ *
+ * Release Date : 11 Apr 2022 
+ *
+ * Contact : info@bitsstech.com
+ *
+ *
+ */
+
+$_GET['fuse'] = 'billing';
+$_GET['action'] = 'gatewaycallback';
+$_GET['plugin'] = 'clientExec-payumoney-payment-plugin';
+chdir('../../..');
+require_once dirname(__FILE__).'/../../../library/front.php';
